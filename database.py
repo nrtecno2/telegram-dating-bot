e869a2c8de9da41294f63a47843df79974eec102
nrtecno2 <privacyofnr@gmail.com>
2026-06-12 07:24:05 +0530

    Update database.py

--- database.py
+++ database.py
@@ -7,6 +7,8 @@ db = sqlite3.connect(
 
 cursor = db.cursor()
 
+# Users Table
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
     user_id INTEGER PRIMARY KEY,
@@ -17,6 +19,39 @@ CREATE TABLE IF NOT EXISTS users(
     about TEXT,
     preference TEXT,
     completed INTEGER DEFAULT 0
+)
+""")
+
+# Media Table
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS media(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    file_id TEXT,
+    media_type TEXT
+)
+""")
+
+# Likes Table
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS likes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    from_user INTEGER,
+    to_user INTEGER
+)
+""")
+
+# Notifications Table
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS notifications(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    type TEXT,
+    data TEXT,
+    status INTEGER DEFAULT 0
 )
 """)
 
