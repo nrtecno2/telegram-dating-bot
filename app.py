a24f1a9724b8b6196caa6d63bb104b7864d3629f
nrtecno2 <privacyofnr@gmail.com>
2026-06-12 07:56:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,95 @@
-from handlers.start import register_start_handler
 import os
 import telebot
 
-from flask import Flask, request
+from flask import Flask
+from flask import request
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+import database
 
-bot = telebot.TeleBot(BOT_TOKEN)
-register_start_handler(bot)
+from handlers.start import register_start_handler
+from handlers.profile import register_profile_handler
+
+
+BOT_TOKEN = os.environ.get(
+    "BOT_TOKEN"
+)
+
+bot = telebot.TeleBot(
+    BOT_TOKEN
+)
 
 app = Flask(__name__)
 
 
-@app.route("/")
+# ==========================
+# REGISTER HANDLERS
+# ==========================
+
+register_start_handler(
+    bot
+)
+
+register_profile_handler(
+    bot
+)
+
+
+# ==========================
+# HOME PAGE
+# ==========================
+
+@app.route(
+    "/"
+)
+
 def home():
-    return "Dating Bot Working Successfully"
+
+    return "Telegram Dating Bot Running Successfully"
 
 
-@app.route(f"/{BOT_TOKEN}", methods=["POST"])
+# ==========================
+# WEBHOOK
+# ==========================
+
+@app.route(
+
+    f"/{BOT_TOKEN}",
+
+    methods=[
+        "POST"
+    ]
+
+)
+
 def webhook():
 
-    json_string = request.get_data().decode("utf-8")
+    json_string = request.get_data().decode(
+        "utf-8"
+    )
 
     update = telebot.types.Update.de_json(
         json_string
     )
 
     bot.process_new_updates(
-        [update]
+        [
+            update
+        ]
     )
 
     return "OK", 200
 
 
-@bot.message_handler(commands=["start"])
-def start(message):
-
-    bot.reply_to(
-        message,
-        "Bot Successfully Connected ✅"
-    )
-
+# ==========================
+# MAIN
+# ==========================
 
 if __name__ == "__main__":
 
     app.run(
+
         host="0.0.0.0",
+
         port=5000
+
     )
