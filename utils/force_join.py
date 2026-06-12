5839abcec6c858b45e3fa66270bdd5351780a18c
nrtecno2 <privacyofnr@gmail.com>
2026-06-12 07:43:42 +0530

    Update force_join.py

--- utils/force_join.py
+++ utils/force_join.py
@@ -1,1 +1,27 @@
+import os
 
+CHANNEL_USERNAME = os.environ.get(
+    "CHANNEL_USERNAME"
+)
+
+
+def check_force_join(bot, user_id):
+
+    try:
+
+        member = bot.get_chat_member(
+            CHANNEL_USERNAME,
+            user_id
+        )
+
+        if member.status in [
+            "member",
+            "administrator",
+            "creator"
+        ]:
+            return True
+
+    except:
+        pass
+
+    return False
